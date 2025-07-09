79c333ddd5bc9a9ae45dec63779270860fd46891
sabbani saikiran <sabbanisaikiran1978@gmail.com>
2025-07-09 23:45:41 +0530

    Initial Maven project

---
+++ src/main/java/com/sabbani/App.java
@@ -0,0 +1,13 @@
+package com.sabbani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
